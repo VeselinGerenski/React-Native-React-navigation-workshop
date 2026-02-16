5da9801f427248f122ef51c7d8625cf6562666ad
VeselinGerenski <veselingerenski@gmail.com>
2026-02-16 09:57:09 +0200

    Add safe area library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+
+      <View style={styles.container}>
+        <Text>Restaurant App</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
+
 }
 
 const styles = StyleSheet.create({
